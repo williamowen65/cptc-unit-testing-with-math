cd00e6a65d966977a37bf5f0ef11478853db8b92
William Owen <william.owen.bsc@gmail.com>
2025-02-16 15:29:53 -0800

    Unit test for SimpleMath class and implmentation

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -18,27 +18,68 @@ namespace CPW211_UnitTestStarterCode.Tests
         [DataRow(0, -0)]
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
-            // Use the DataRow values to test the Add method
-            Assert.Fail();
+            // Arrange
+            double expected = num1 + num2;
+            // Act
+            double actual = SimpleMath.Add(num1, num2);
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double a, double b)
         {
-            // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            // Arrange
+            double expected = a * b;
+            // Act
+            double actual = SimpleMath.Multiply(a, b);
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Divide_DenominatorZero_ThrowsArgumentException()
+        [DataRow(5, 0)]
+        [DataRow(0, 0)]
+        [DataRow(-1, 0)]
+        [DataRow(0, -0)]
+        public void Divide_DenominatorZero_ThrowsArgumentException(double a, double b)
         {
-            // Divide by zero should throw an argument exception with a message
-            // "Denominator cannot be zero"
-            Assert.Fail();
+            // Arrange
+            // Assert => Act
+            Assert.ThrowsException<ArgumentException>(() => SimpleMath.Divide(a, b));
         }
 
-        // TODO: Test Divide method with two valid numbers
 
-        // TODO: Test subtract method with two valid numbers
+        [TestMethod]
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        public void Divide_WithValidNumbers_ReturnsResult(double a, double b)
+        {
+            // Arrange
+            double expected = a / b;
+            // Act
+            double actual = SimpleMath.Divide(a, b);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            // Arrange
+            double a = 5;
+            double b = 10;
+            double expected = a - b;
+            // Act
+            double actual = SimpleMath.Subtract(a, b);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
